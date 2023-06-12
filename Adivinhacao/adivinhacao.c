3b281e86a74f7e7f0f42a9a66175317232aef21c
Gabriel Baruque <baruque@edu.unirio.br>
2023-06-12 12:18:05 -0300

    Jogo finalizado

--- Adivinhacao/adivinhacao.c
+++ Adivinhacao/adivinhacao.c
@@ -8,47 +8,83 @@ int main()
 	// Imprimindo o cabecalho do jogo
 	printf("************************************\n");
 	printf("* Bem-vindo ao jogo da adivinhacao *\n");
-	printf("************************************\n");
+	printf("************************************\n\n");
 
 	int segundos = time(0);
 	srand(segundos);
 	int numerosecreto = rand() % 100; // Numero secreto ainda nao randomizado
 	int chute;
 	int pontuacao = 1000;
-	int adivinhou = 0;
 	int tentativa = 1;
+	int max_attempts;
+	int dificuldade;
+
+	printf("Escolha um nivel de dificuldade:\n");
+	printf("1 - FACIL\n");
+	printf("2 - MEDIO\n");
+	printf("3 - DIFICIL\n\n");
+	scanf("%d", &dificuldade);
+
+	while (dificuldade < 1 || dificuldade > 3)
+	{
+		printf("Número inválido. Tente novamente.\n");
+		printf("Escolha um nivel de dificuldade:\n");
+		printf("1 - FACIL\n");
+		printf("2 - MEDIO\n");
+		printf("3 - DIFICIL\n");
+
+		scanf("%d", &dificuldade);
+
+		if (dificuldade < 1 || dificuldade > 3)
+		{
+			break;
+		}
+	}
+
+	switch (dificuldade)
+	{
+	case 1:
+		max_attempts = 15;
+	case 2:
+		max_attempts = 10;
+	case 3:
+		max_attempts = 5;
+	}
 
 	// Tentativas até zerar a pontuação do jogo.
 
-	while (adivinhou == 0) // Loop while com problema!
+	for (int i = 0; i < max_attempts; i++) // Loop while com problema!
 	{
-		printf("Tentativa %d: Digite um numero: ", tentativa);
+		printf("\nTentativa %d: Digite um numero:  ", tentativa);
 		scanf("%d", &chute);
 
 		if (chute == numerosecreto)
 		{
 			printf("O numero %d eh o numero secreto! Parabens!\n", chute);
 			printf("Voce fez %d pontos e acertou na %da tentativa.", pontuacao, tentativa);
-			adivinhou = 1;
+			getchar();
+			break;
 		}
 		else
 		{
 			if (chute > numerosecreto)
 			{
-				printf("O numero %d eh maior que o numero secreto... Tente novamente:\n", chute);
+				printf("O numero %d eh maior que o numero secreto...\n", chute);
 				pontuacao -= (chute - numerosecreto);
 				tentativa++;
 			}
 			else
 			{
-				printf("O numero %d eh menor que o numero secreto... Tente novamente:\n", chute);
+				printf("O numero %d eh menor que o numero secreto...\n", chute);
 				pontuacao -= (numerosecreto - chute);
 				tentativa++;
 			}
 		}
+	}
 
-		if (pontuacao == 0)
-			printf("Você perdeu o jogo...");
-		break;
+	if (chute != numerosecreto)
+	{
+		printf("\n\nSuas tentativas acabaram e voce perdeu o jogo...\n\n");
+		getchar();
 	}
 }
